1947e55cd5245e6998f7b81fa9e4237c057b20bb
sayantanwise <sayantansinha575@gmail.com>
2026-03-13 13:28:19 +0530

    update-3 (13-03-26)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -38,7 +38,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 console.log("2");
 
 // Auth Routes
